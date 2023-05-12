b40505074e07be76c5b78ac436cadf04f127be2a
ASUS <phivip123123@gmail.com>
2023-05-12 17:17:09 +0700

    hello

---
+++ src/main/java/xmlbaseconfiguration/Main.java
@@ -0,0 +1,24 @@
+package xmlbaseconfiguration;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pojobeans.AccountService;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main (String[] args)
+    {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("xmlbaseconfiguration/beans.xml");
+        AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+
+        System.out.println("Before money transfer");
+        System.out.println("Account 1 balance :"+ accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance :"+ accountService.getAccount(2).getBalance());
+
+        accountService.transferMoney(1, 2, 5.0);
+        System.out.println("===================");
+        System.out.println("After money transfer");
+        System.out.println("Account 1 balance :"+ accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance :"+ accountService.getAccount(2).getBalance());
+    }
+}
